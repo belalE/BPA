c4f23d3a5d2c3276da92c45ea69ce5ee261787fd
Belal E <balolyman1234@gmail.com>
2018-11-02 16:14:50 -0500

    Set up sign up and login

--- BPA/ViewController.swift
+++ BPA/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func loginPressed(_ sender: UIButton) {
+        if let  email = emailTextField.text, let pass = passwordTextField.text {
+            
+        
+        Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "Login", sender: self)
+            } else {
+                // error
+            }
+        }
+        }
+        }
+    
+    
+    
+    @IBAction func signUpPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let pass = passwordTextField.text {
+        Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+            if user != nil {
+                //user is found, go to next screen
+                self.performSegue(withIdentifier: "setUpProfile", sender: self)
+            } else {
+                //error
+            }
+        }
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //dismiss the keyboard when view is tapped on
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
 }
+    
+
 
